78cc6899165d9afe6cdc967ceb6797515844722a
Nicolas Goutay <nicolas.goutay@gmail.com>
2022-11-28 16:16:21 +0000

    Add repro

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @ping_issue_click_counter ||= 0
+
+    @full_text_of_moby_dick ||= "This is going to be the full text of Moby Dick (~1Mb)"
   end
 end
